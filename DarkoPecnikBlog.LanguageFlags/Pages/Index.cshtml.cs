5770134dc871861e8445ac4e3eddaca23db4b257
Darko Pećnik <darko.pecnik@live.com>
2022-04-04 08:10:54 +0200

    Add project files.

---
+++ DarkoPecnikBlog.LanguageFlags/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+
+namespace DarkoPecnikBlog.LanguageFlags.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IStringLocalizer<IndexModel> _localizer;
+
+        public IndexModel(IStringLocalizer<IndexModel> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = _localizer["Title"];
+        }
+
+        public IActionResult OnPostSetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(
+                    new RequestCulture(culture)),
+                    new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
---
+++ DarkoPecnikBlog.LanguageFlags/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+// Add services to the container.
+builder.Services.AddRazorPages().AddViewLocalization();
+
+var supportedCultures = new[] { "en-GB", "de-DE", "fr-FR", "hr-HR", "fi-FI", "sv-SE", "uk-UA" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+builder.Services.AddRequestLocalization(options => {
+    options.DefaultRequestCulture = localizationOptions.DefaultRequestCulture;
+    options.SupportedCultures = localizationOptions.SupportedCultures;
+    options.SupportedUICultures = localizationOptions.SupportedUICultures;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseRequestLocalization(localizationOptions);
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
